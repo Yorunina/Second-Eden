25ceb4e50c66c487851571f4417dc4537ec21668
YoruNina <836193631@qq.com>
2024-07-05 17:01:03 +0800

    补充遗失部分汉化

--- kubejs/startup_scripts/src/register/common/item.js
+++ kubejs/startup_scripts/src/register/common/item.js
@@ -1,4 +1,20 @@
 StartupEvents.registry('item', event => {
     event.create('echo_crystal', 'basic').texture('kubejs:item/echo_crystal')
     event.create('coal_coke', 'basic').burnTime(3200).texture('kubejs:item/coal_coke')
+    event.create('infinity_cooked_beef', 'basic')
+        .useAnimation('eat')
+        .use((level, player, hand) => {
+            return true;
+        })
+        .useDuration(itemStack => 20)
+        .finishUsing((itemstack, level, entity) => {
+            if (level.isClientSide()) return itemstack
+            if (!entity.isPlayer()) return itemstack
+            /**@type {$ServerPlayer_} */
+            let player = entity
+            player.eat(level, 'minecraft:cooked_beef')
+            player.addItemCooldown(itemstack.id, 20 * 60)
+            return itemstack
+        })
+        .texture('kubejs:item/infinity_cooked_beef')
 })
