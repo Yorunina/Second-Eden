54f8353a1d123104a88960499c7f4f130ee0a5fd
Neha YoruNina <836193631@qq.com>
2024-08-23 02:06:31 +0800

    修复部分配方问题

--- kubejs/server_scripts/src/colony/recipe/overlimit.js
+++ kubejs/server_scripts/src/colony/recipe/overlimit.js
@@ -9,107 +9,107 @@ ServerEvents.highPriorityData(event => {
             .setResearchId('kubejs:effects/singulize', true))
 
     event.addJson(`kubejs:crafterrecipes/blacksmith/singularity_copper.json`,
-        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_copper'), [Item.of('minecraft:copper_ingot', 1024)])
+        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_copper'), [Item.of('minecraft:copper_ingot', 64)])
             .setTool('singulize_potion')
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/singulize', true))
 
     event.addJson(`kubejs:crafterrecipes/blacksmith/singularity_iron.json`,
-        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_iron'), [Item.of('minecraft:iron_ingot', 1024)])
+        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_iron'), [Item.of('minecraft:iron_ingot', 64)])
             .setTool('singulize_potion')
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/singulize', true))
 
     event.addJson(`kubejs:crafterrecipes/blacksmith/singularity_gold.json`,
-        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_gold'), [Item.of('minecraft:gold_ingot', 1024)])
+        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_gold'), [Item.of('minecraft:gold_ingot', 64)])
             .setTool('singulize_potion')
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/singulize', true))
 
     event.addJson(`kubejs:crafterrecipes/blacksmith/singularity_netherite.json`,
-        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_netherite'), [Item.of('minecraft:netherite_ingot', 1024)])
+        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_netherite'), [Item.of('minecraft:netherite_ingot', 64)])
             .setTool('singulize_potion')
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/singulize', true))
 
     event.addJson(`kubejs:crafterrecipes/blacksmith/singularity_diamond.json`,
-        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_diamond'), [Item.of('minecraft:diamond', 1024)])
+        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_diamond'), [Item.of('minecraft:diamond', 64)])
             .setTool('singulize_potion')
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/singulize', true))
 
     event.addJson(`kubejs:crafterrecipes/blacksmith/singularity_emerald.json`,
-        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_emerald'), [Item.of('minecraft:emerald', 1024)])
+        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_emerald'), [Item.of('minecraft:emerald', 64)])
             .setTool('singulize_potion')
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/singulize', true))
 
     event.addJson(`kubejs:crafterrecipes/blacksmith/singularity_amethyst.json`,
-        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_amethyst'), [Item.of('minecraft:amethyst_shard', 1024)])
+        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_amethyst'), [Item.of('minecraft:amethyst_shard', 64)])
             .setTool('singulize_potion')
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/singulize', true))
 
     event.addJson(`kubejs:crafterrecipes/blacksmith/singularity_prismarine.json`,
-        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_prismarine'), [Item.of('minecraft:prismarine_shard', 1024)])
+        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_prismarine'), [Item.of('minecraft:prismarine_shard', 64)])
             .setTool('singulize_potion')
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/singulize', true))
 
     event.addJson(`kubejs:crafterrecipes/blacksmith/singularity_quartz.json`,
-        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_quartz'), [Item.of('minecraft:quartz', 1024)])
+        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_quartz'), [Item.of('minecraft:quartz', 64)])
             .setTool('singulize_potion')
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/singulize', true))
 
     event.addJson(`kubejs:crafterrecipes/blacksmith/singularity_lapis.json`,
-        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_lapis'), [Item.of('minecraft:lapis_lazuli', 1024)])
+        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_lapis'), [Item.of('minecraft:lapis_lazuli', 64)])
             .setTool('singulize_potion')
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/singulize', true))
 
     event.addJson(`kubejs:crafterrecipes/blacksmith/singularity_redstone.json`,
-        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_redstone'), [Item.of('minecraft:redstone', 1024)])
+        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_redstone'), [Item.of('minecraft:redstone', 64)])
             .setTool('singulize_potion')
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/singulize', true))
 
     event.addJson(`kubejs:crafterrecipes/blacksmith/singularity_glowstone.json`,
-        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_glowstone'), [Item.of('minecraft:glowstone_dust', 1024)])
+        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_glowstone'), [Item.of('minecraft:glowstone_dust', 64)])
             .setTool('singulize_potion')
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/singulize', true))
 
     event.addJson(`kubejs:crafterrecipes/blacksmith/singularity_coal.json`,
-        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_coal'), [Item.of('minecraft:coal', 1024)])
+        new ColonyCraftRecipes('blacksmith_crafting', Item.of('kubejs:singularity_coal'), [Item.of('minecraft:coal', 64)])
             .setTool('singulize_potion')
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/singulize', true))
 
     // 无穷无尽多维理论
     event.addJson(`kubejs:crafterrecipes/mechanic/creative_storage_disk.json`,
-        new ColonyCraftRecipes('mechanic_crafting', Item.of('refinedstorage:creative_storage_disk', 1), [Item.of('refinedstorage:64k_storage_part', 64), Item.of('refinedstorage:silicon', 128), Item.of('kubejs:echo_crystal', 16), Item.of('refinedstorage:storage_housing')])
+        new ColonyCraftRecipes('mechanic_crafting', Item.of('refinedstorage:creative_storage_disk', 1), [Item.of('refinedstorage:64k_storage_part', 64), Item.of('refinedstorage:silicon', 64), Item.of('kubejs:echo_crystal', 16), Item.of('refinedstorage:storage_housing')])
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/creativediskunlock', true))
 
     event.addJson(`kubejs:crafterrecipes/mechanic/creative_fluid_storage_disk.json`,
-        new ColonyCraftRecipes('mechanic_crafting', Item.of('refinedstorage:creative_fluid_storage_disk', 1), [Item.of('refinedstorage:64k_storage_part', 64), Item.of('minecraft:glass', 128), Item.of('kubejs:echo_crystal', 16), Item.of('refinedstorage:storage_housing')])
+        new ColonyCraftRecipes('mechanic_crafting', Item.of('refinedstorage:creative_fluid_storage_disk', 1), [Item.of('refinedstorage:64k_storage_part', 64), Item.of('minecraft:glass', 64), Item.of('kubejs:echo_crystal', 16), Item.of('refinedstorage:storage_housing')])
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/creativediskunlock', true))
 
     event.addJson(`kubejs:crafterrecipes/mechanic/creative_storage_block.json`,
-        new ColonyCraftRecipes('mechanic_crafting', Item.of('refinedstorage:creative_storage_block', 1), [Item.of('refinedstorage:64k_storage_part', 64), Item.of('refinedstorage:silicon', 128), Item.of('kubejs:echo_crystal', 16), Item.of('refinedstorage:machine_casing')])
+        new ColonyCraftRecipes('mechanic_crafting', Item.of('refinedstorage:creative_storage_block', 1), [Item.of('refinedstorage:64k_storage_part', 64), Item.of('refinedstorage:silicon', 64), Item.of('kubejs:echo_crystal', 16), Item.of('refinedstorage:machine_casing')])
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/creativediskunlock', true))
 
     event.addJson(`kubejs:crafterrecipes/mechanic/creative_fluid_storage_block.json`,
-        new ColonyCraftRecipes('mechanic_crafting', Item.of('refinedstorage:creative_fluid_storage_block', 1), [Item.of('refinedstorage:64k_storage_part', 64), Item.of('minecraft:glass', 128), Item.of('kubejs:echo_crystal', 16), Item.of('refinedstorage:machine_casing')])
+        new ColonyCraftRecipes('mechanic_crafting', Item.of('refinedstorage:creative_fluid_storage_block', 1), [Item.of('refinedstorage:64k_storage_part', 64), Item.of('minecraft:glass', 64), Item.of('kubejs:echo_crystal', 16), Item.of('refinedstorage:machine_casing')])
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/creativediskunlock', true))
 
     // 无限无线通讯协议
     event.addJson(`kubejs:crafterrecipes/enchanter/infinity_card.json`,
-        new ColonyCraftRecipes('enchanter_custom', Item.of('rsinfinitybooster:infinity_card', 1), [Item.of('refinedstorage:network_card', 16), Item.of('kubejs:echo_crystal', 16), Item.of('minecraft:chorus_fruit', 128)])
+        new ColonyCraftRecipes('enchanter_custom', Item.of('rsinfinitybooster:infinity_card', 1), [Item.of('refinedstorage:network_card', 16), Item.of('kubejs:echo_crystal', 16), Item.of('minecraft:chorus_fruit', 64)])
             .setMinBuildingLevel(5)
             .setResearchId('kubejs:effects/infinitywirelessaccess', true))
 
