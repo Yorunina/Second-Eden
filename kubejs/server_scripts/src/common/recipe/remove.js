16db4a5c2a3062af287dbadff8666272231cfed7
Neha YoruNina <836193631@qq.com>
2024-09-05 03:44:38 +0800

    引入神匠药水

--- kubejs/server_scripts/src/common/recipe/remove.js
+++ kubejs/server_scripts/src/common/recipe/remove.js
@@ -1,7 +1,4 @@
 // priority: 1000
 ServerEvents.recipes(event => {
     event.remove({ output: 'bountiful:bountyboard' })
-    event.remove({ output: 'farmingforblockheads:red_fertilizer' })
-    event.remove({ output: 'farmingforblockheads:yellow_fertilizer' })
-    event.remove({ output: 'farmingforblockheads:green_fertilizer' })
 })
--- kubejs/startup_scripts/src/register/common/potion.js
+++ kubejs/startup_scripts/src/register/common/potion.js
@@ -6,7 +6,16 @@ StartupEvents.registry('mob_effect', event => {
         .color(0xFB8A00)
 })
 
-
 StartupEvents.registry('potion', event => {
-    event.createCustom('kubejs:creative_builder', () => new $PotionBuilder('kubejs:my_potion').effect('kubejs:test', 300).createObject())
+    event.createCustom('kubejs:creative_builder', () => new $PotionBuilder('kubejs:creative_builder').effect('kubejs:creative_builder', 6000).createObject())
+
+    event.createCustom('kubejs:creative_builder_1', () => new $PotionBuilder('kubejs:creative_builder').effect('kubejs:creative_builder', 12000).createObject())
+
+    event.createCustom('kubejs:creative_builder_2', () => new $PotionBuilder('kubejs:creative_builder').effect('kubejs:creative_builder', 24000).createObject())
 });
+
+
+MoreJSEvents.registerPotionBrewing(event => {
+    event.addPotionBrewing('minecraft:sniffer_egg', 'kubejs:creative_builder', 'kubejs:creative_builder_1')
+    event.addPotionBrewing('splat:splat_scale', 'kubejs:creative_builder_1', 'kubejs:creative_builder_2')
+})
